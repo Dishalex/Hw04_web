56efaf03efff1fdb8043c950d7136f395cac8143
Dishalex <dishalex@gmail.com>
2023-09-28 15:56:23 +0300

    final changes

--- main.py
+++ main.py
@@ -8,20 +8,22 @@ import logging
 from threading import Thread
 import socket
 
-BASE_DIR = pathlib.Path()
+BASE_DIR = pathlib.Path('application')
+STORAGE_DIR = BASE_DIR.joinpath('storage')
+FILE_STORAGE = STORAGE_DIR.joinpath('data.json')
+
 RESPONSE_OK = 200
 RESPONSE_BAD = 404
 RESPONSE_REDIR = 302
 PORT = 3000
 PORT_SOCKET = 5000
 IP = '127.0.0.1'
+IP_OUT = '0.0.0.0'
 BUFFER = 1024
 
 
 def send_data_to_socket(body):
-    print('send_data_to_socket body', body)
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print("client_socket", client_socket)
     client_socket.sendto(body, (IP, PORT_SOCKET))
     
     client_socket.close()
@@ -32,7 +34,6 @@ class HTTPHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         body = self.rfile.read(int(self.headers['Content-Length']))
-        print('do_POST body', body)
         send_data_to_socket(body)
         
         self.send_response(RESPONSE_REDIR)
@@ -45,17 +46,15 @@ class HTTPHandler(BaseHTTPRequestHandler):
      
         match route.path:
             case '/':
-                self.send_html('index.html')
+                self.send_html(BASE_DIR / 'index.html')
             case '/message.html':
-                self.send_html('message.html')           
+                self.send_html(BASE_DIR / 'message.html')           
             case _:
                 file = BASE_DIR / route.path[1:]
                 if file.exists():
                     self.send_static(file)
                 else:
-                    self.send_html('error.html', RESPONSE_BAD)
-
-
+                    self.send_html(BASE_DIR / 'error.html', RESPONSE_BAD)
 
 
     def send_html(self, filename, status_code=RESPONSE_OK):
@@ -80,8 +79,7 @@ class HTTPHandler(BaseHTTPRequestHandler):
 
 
 def run(server=HTTPServer, handler=HTTPHandler):
-    address = (IP, PORT)
-    print('84 run, address - ', address)
+    address = (IP_OUT, PORT)
     http_server = server(address, handler)
     try:
         http_server.serve_forever()
@@ -91,12 +89,21 @@ def run(server=HTTPServer, handler=HTTPHandler):
 
 def save_data(data):
     body = urllib.parse.unquote_plus(data.decode())
-    print(f'body from save_data {body}')
+
     try:
-        payload = {str(datetime.now()): {k: v for k, v in [el.split('=') for el in body.split('&')]}}
-        print(payload)
-        with open(BASE_DIR.joinpath('storage/data.json'), 'w', encoding='utf-8') as fd:
-            json.dump(payload, fd, ensure_ascii=False)
+        payload = {str(datetime.now()): {k: v.strip() for k, v in [el.split('=') for el in body.split('&')]}}
+
+        try:
+            with open(FILE_STORAGE, 'r', encoding='utf-8') as fd:
+                data = json.load(fd)
+                data.update(payload)
+                payload = data
+        except Exception:
+            pass
+        finally:
+            with open(FILE_STORAGE, 'w', encoding='utf-8') as fd:
+                json.dump(payload, fd)
+
     except ValueError as err:
         logging.error(f'Field parse data {body} with error: {err}')
     except OSError as err:
@@ -104,18 +111,14 @@ def save_data(data):
 
 
 def run_socket_server(ip, port):
-    print('107 run_socket_server')
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server = ip, port
-    print("server", server)
     server_socket.bind(server)
     
     try:
         while True:
             data, address = server_socket.recvfrom(BUFFER)
-            print(f"run_socket_server with data: {data}, address: {address}")
             save_data(data)
-            print(f'Data {data} from address {address} saved')
     except KeyboardInterrupt:
         logging.info('Socket server stopped')
     finally:
@@ -126,10 +129,12 @@ def run_socket_server(ip, port):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(threadName)s %(message)s')
 
+    if not FILE_STORAGE.exists():
+        with open(FILE_STORAGE, 'w', encoding='utf-8') as fd:
+            json.dump({}, fd)
+
     thread_server = Thread(target=run)
     thread_server.start()
 
     thread_socket = Thread(target=run_socket_server(IP, PORT_SOCKET))
     thread_socket.start()
-
-
